ea7e865a7b85a1b1e3d10dc2fda85e50377b75b4
Tristan Ravitch <travitch@cs.wisc.edu>
2011-05-22 12:09:26 -0500

    Stub out the test suite using Cabal's built-in testing support (and HUnit).
    The detailed test runner doesn't seem to work yet so I am using
    test-framework and HUnit.
    
    HUnit wanted a Show instance for ROBDDs, so I also added one of those
    built on graphviz.  Not thrilled about that but it works.

--- src/Data/ROBDD/Strict/Types.hs
+++ src/Data/ROBDD/Strict/Types.hs
@@ -4,6 +4,8 @@ module Data.ROBDD.Strict.Types ( BDD(..)
                                , Var
                                , RevMap
                                , Map
+                               , DAG
+                               , makeDAG
                                , bddCmp
                                , highEdge
                                , lowEdge
@@ -13,6 +15,8 @@ module Data.ROBDD.Strict.Types ( BDD(..)
 
 import Data.GraphViz
 import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as M
+import Data.Graph.Inductive (Gr, mkGraph)
 
 type Map = HashMap
 
@@ -20,6 +24,8 @@ type RevMap = Map (Var, NodeId, NodeId) BDD
 
 type NodeId = Int
 type Var = Int
+
+type DAG = Gr BDD Bool
 
 -- Node types FIXME: Add another Int field to each BDD node to act as
 -- a hash code.  Assign values to Zero and One, then the hash code for
@@ -35,6 +41,9 @@ data BDD = BDD BDD Var BDD NodeId
 
 -- Accessible wrapper
 data ROBDD = ROBDD RevMap [Int] BDD
+
+instance Show ROBDD where
+  show bdd = prettyPrint' $ graphToDot nonClusteredParams $ makeDAG bdd
 
 instance Eq BDD where
   Zero == Zero = True
@@ -82,3 +91,33 @@ nodeUID :: BDD -> Int
 nodeUID Zero = (-1)
 nodeUID One = (-2)
 nodeUID (BDD _ _ _ uid) = uid
+
+makeDAG :: ROBDD -> DAG
+makeDAG (ROBDD _ _ bdd) = mkGraph nodeList (map unTuple $ M.toList edges)
+  where
+    nodes :: Map Var BDD
+    nodes = collectNodes bdd M.empty
+    nodeList :: [ (Var, BDD) ]
+    nodeList = M.toList nodes
+    collectNodes :: BDD -> Map Var BDD -> Map Var BDD
+    collectNodes b@(BDD low v high _) s =
+      let s' = collectNodes low s
+          s'' = collectNodes high s'
+      in M.insert v b s''
+    collectNodes Zero s = M.insert (-1) Zero s
+    collectNodes One s = M.insert (-2) One s
+    edges :: Map (Var, Var) Bool
+    edges = collectEdges bdd M.empty
+    collectEdges :: BDD -> Map (Var, Var) Bool -> Map (Var, Var) Bool
+    collectEdges (BDD low v high _) s =
+      let s' = collectEdges low s
+          s'' = collectEdges high s'
+          s''' = M.insert (v, bddVarNum low) False s''
+      in M.insert (v, bddVarNum high) True s'''
+    collectEdges _ s = s
+    unTuple ((a, b), c) = (a, b, c)
+
+    bddVarNum :: BDD -> Var
+    bddVarNum Zero = -1
+    bddVarNum One = -2
+    bddVarNum (BDD _ v _ _) = v
--- tests/QuantTests.hs
+++ tests/QuantTests.hs
@@ -1,32 +1,51 @@
-import System.Random
-import Prelude hiding (and, or)
+import Test.Framework ( defaultMain, testGroup )
+import Test.Framework.Providers.HUnit
+import Test.HUnit
 
-import Data.ROBDD.Strict
-import Data.ROBDD.Strict.Visualization
-
-makeFormula :: [Bool] -> [ROBDD] -> ROBDD
-makeFormula ops (var:vars) =
-  foldr combine var (zip vars ops)
-  where combine (v, op) f =
-          if op then and f v else or f v
-makeFormula ops _ = error "Need at least one var"
+import qualified Data.ROBDD.Strict as BDD
 
 main :: IO ()
-main = do
-  g <- getStdGen
-  let rs = randoms g
-      (rs1, rs2) = splitAt 100 rs
-      logicVars = take 100 $ map makeVar [0..]
-      f1 = makeFormula rs1 logicVars
-      f2 = makeFormula rs2 logicVars
-      f3 = and f1 f2
-      f4 = exist f3 16
-      f5 = applyExists (&&) f1 f2 [16]
-      dag1 = makeDAG f4
-      dag2 = makeDAG f5
-      sat = anySat f5
+main = defaultMain tests
 
-  viewDAG dag1
-  viewDAG dag2
+-- tests :: [Test]
+tests = [ testGroup "Tautologies" [
+             testCase "taut1" test_taut1
+             ]
+        ]
 
-  print sat
+test_taut1 = assertEqual "x[1] || x[2] ?? x[1] / True" (BDD.restrict bdd 1 True) BDD.makeTrue
+  where
+    bdd = (BDD.makeVar 1) `BDD.or` (BDD.makeVar 2)
+
+-- import System.Random
+-- import Prelude hiding (and, or)
+
+-- import Data.ROBDD.Strict
+-- import Data.ROBDD.Strict.Visualization
+
+-- makeFormula :: [Bool] -> [ROBDD] -> ROBDD
+-- makeFormula ops (var:vars) =
+--   foldr combine var (zip vars ops)
+--   where combine (v, op) f =
+--           if op then and f v else or f v
+-- makeFormula ops _ = error "Need at least one var"
+
+-- main :: IO ()
+-- main = do
+--   g <- getStdGen
+--   let rs = randoms g
+--       (rs1, rs2) = splitAt 100 rs
+--       logicVars = take 100 $ map makeVar [0..]
+--       f1 = makeFormula rs1 logicVars
+--       f2 = makeFormula rs2 logicVars
+--       f3 = and f1 f2
+--       f4 = exist f3 16
+--       f5 = applyExists (&&) f1 f2 [16]
+--       dag1 = makeDAG f4
+--       dag2 = makeDAG f5
+--       sat = anySat f5
+
+--   viewDAG dag1
+--   viewDAG dag2
+
+--   print sat
